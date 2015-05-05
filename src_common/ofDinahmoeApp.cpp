ee026a2917ef0ef07fa289c516edbc24e482788a
Alessandro Saccoia <alex@keplero.com>
2015-05-05 12:44:10 +0200

    First commit

---
+++ src_common/ofDinahmoeApp.cpp
@@ -0,0 +1,116 @@
+#include "ofDinahmoeApp.h"
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
+
+#ifdef OF_TARGET_IOS
+
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::touchDown(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::touchMoved(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::touchUp(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::touchDoubleTap(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::touchCancelled(ofTouchEventArgs & touch){
+    
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::lostFocus(){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::gotFocus(){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::gotMemoryWarning(){
+
+}
+
+//--------------------------------------------------------------
+void ofDinahmoeApp::deviceOrientationChanged(int newOrientation){
+
+}
+
+#endif
